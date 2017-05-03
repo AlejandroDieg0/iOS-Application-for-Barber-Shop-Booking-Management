df7e7c3a1545f9067d5f8dc190144f34270bd06d
Alessandro <alessandrocascino@10-20-48-194.0200.fed.it.iosda.org>
2017-05-03 16:41:00 +0200

    First Commit

---
+++ sigabrtProject/sigabrtProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sigabrtProject
+//
+//  Created by Alessandro Cascino on 03/05/17.
+//  Copyright © 2017 Alessandro Cascino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
