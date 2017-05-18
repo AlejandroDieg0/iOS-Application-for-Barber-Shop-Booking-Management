44743f8897640beaa47c155bcb9ee7410da8fe7f
fcindolo <f.cindolo@studenti.unina.it>
2017-05-18 15:18:31 +0200

    Fixed Storage and other pods

--- sigabrtProject/Pods/TextFieldEffects/TextFieldEffects/TextFieldEffects/YokoTextField.swift
+++ sigabrtProject/Pods/TextFieldEffects/TextFieldEffects/TextFieldEffects/YokoTextField.swift
@@ -65,7 +65,7 @@ import UIKit
     private let placeholderInsets = CGPoint(x: 6, y: 6)
     private let textFieldInsets = CGPoint(x: 6, y: 6)
     
-    // MARK: - TextFieldEffects
+    // MARK: - TextFieldsEffects
     
     override open func drawViewsForRect(_ rect: CGRect) {
         updateForeground()
@@ -200,7 +200,7 @@ import UIKit
         
         var rotationAndPerspectiveTransform = CATransform3DIdentity
         rotationAndPerspectiveTransform.m34 = 1.0/800
-        let radians = ((-90) / 180.0 * CGFloat.pi)
+        let radians = ((-90) / 180.0 * CGFloat(M_PI))
         rotationAndPerspectiveTransform = CATransform3DRotate(rotationAndPerspectiveTransform, radians, 1.0, 0.0, 0.0)
         return rotationAndPerspectiveTransform
     }
