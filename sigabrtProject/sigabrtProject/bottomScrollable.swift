761ba2cf3289caadfc933035dcda6e00fc48e31d
Luigi Faticoso <luxor320@gmail.com>
2017-05-26 01:04:59 +0200

    ora la scrollable view si ferma in posti stabiliti

--- sigabrtProject/sigabrtProject/bottomScrollable.swift
+++ sigabrtProject/sigabrtProject/bottomScrollable.swift
@@ -48,14 +48,37 @@ class bottomScrollable: UIViewController{
 //            view.insertSubview(bluredView, at: 0)
 //        }
     
-        func panGesture(recognizer: UIPanGestureRecognizer) {
-            let translation = recognizer.translation(in: self.view)
-            let y = self.view.frame.minY
-            self.view.frame = CGRect(x: 0,y: y + translation.y,width: view.frame.width,height: view.frame.height)
+    func panGesture(_ recognizer: UIPanGestureRecognizer) {
+        
+        let translation = recognizer.translation(in: self.view)
+        let velocity = recognizer.velocity(in: self.view)
+        
+        let y = self.view.frame.minY
+        if (y + translation.y >= fullView) && (y + translation.y <= partialView) {
+            self.view.frame = CGRect(x: 0, y: y + translation.y, width: view.frame.width, height: view.frame.height)
             recognizer.setTranslation(CGPoint.zero, in: self.view)
         }
         
-        
+        if recognizer.state == .ended {
+            var duration =  velocity.y < 0 ? Double((y - fullView) / -velocity.y) : Double((partialView - y) / velocity.y )
+            
+            duration = duration > 1.3 ? 1 : duration
+            
+            UIView.animate(withDuration: duration, delay: 0.0, options: [.allowUserInteraction], animations: {
+                if  velocity.y >= 0 {
+                    self.view.frame = CGRect(x: 0, y: self.partialView, width: self.view.frame.width, height: self.view.frame.height)
+                } else {
+                    self.view.frame = CGRect(x: 0, y: self.fullView, width: self.view.frame.width, height: self.view.frame.height)
+                }
+                
+            }, completion: { [weak self] _ in
+                if ( velocity.y < 0 ) {
+                    self?.tableView.isScrollEnabled = true
+                }
+            })
+        }
+    }
+    
         override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
 //            prepareBackgroundView()
@@ -69,7 +92,6 @@ class bottomScrollable: UIViewController{
             tableView.dataSource = self
             tableView.register(UINib(nibName: "DefaultTableViewCell", bundle: nil), forCellReuseIdentifier: "default")
             
-
             let gesture = UIPanGestureRecognizer.init(target: self, action: #selector(bottomScrollable.panGesture))
             gesture.delegate = self
             view.addGestureRecognizer(gesture)
