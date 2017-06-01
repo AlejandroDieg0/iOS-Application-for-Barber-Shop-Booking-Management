81e894868e92f184958b94a6974ee1866d94be7a
fcindolo <f.cindolo@studenti.unina.it>
2017-06-01 13:17:54 +0200

    Selection pic added

--- sigabrtProject/sigabrtProject/addModifyCollectionViewCell.swift
+++ sigabrtProject/sigabrtProject/addModifyCollectionViewCell.swift
@@ -7,4 +7,5 @@ class addModifyCollectionViewCell: UITableViewCell {
     @IBOutlet weak var servizio: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var imageSelection: UIImageView!
+    @IBOutlet weak var durationLabel: UILabel!
 }
