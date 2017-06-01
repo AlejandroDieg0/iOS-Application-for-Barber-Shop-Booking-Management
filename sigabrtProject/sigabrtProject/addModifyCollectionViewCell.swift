ec6f4a7950450922ee488b36ab7afaf6aaeb5f84
Alessandro <a.cascino-apple@unina.it>
2017-06-01 12:51:06 +0200

    provola

--- sigabrtProject/sigabrtProject/addModifyCollectionViewCell.swift
+++ sigabrtProject/sigabrtProject/addModifyCollectionViewCell.swift
@@ -6,4 +6,5 @@ class addModifyCollectionViewCell: UITableViewCell {
     
     @IBOutlet weak var servizio: UILabel!
     @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var imageSelection: UIImageView!
 }
